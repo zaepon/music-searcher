dff2f791b14042fe728ff1150244fb60f226ab93
Henri Lahti <miokillian@gmail.com>
2019-08-22 14:34:23 +0300

    main container changes

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import Header from '../components/header';
 import Card from '../components/card';
+import { loadSimilarArtists } from '../store/effects';
+import {connect } from 'react-redux';
 
-function App() {
+
+interface AppProps {
+  getSimilarArtists: (name: string) => object
+}
+
+
+function App(props: AppProps) {
   return (
     <div className="App">
       <Header type="h1" title="test" />
-      <Card onClick={() => console.log('test')} text="test" title="test2" img="https://lastfm-img2.akamaized.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png" />
+      <Card onClick={() => props.getSimilarArtists('cher')} text="test" title="test2" img="https://lastfm-img2.akamaized.net/i/u/174s/2a96cbd8b46e442fc41c2b86b821562f.png" />
     </div>
   );
 }
 
-export default App;
+function mapStateToProps(state = []){
+  console.log('state', state);
+}
+
+
+export default connect(mapStateToProps, {getSimilarArtists: loadSimilarArtists})(App);
