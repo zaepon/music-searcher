04c868a7cf0c2c06df25497c6ac11f672fa33b6a
Henri Lahti <miokillian@gmail.com>
2019-08-23 00:11:38 +0300

    loader component

---
+++ src/components/loader.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const spin = keyframes`
+  0% { transform: rotate(0deg); }
+  100% { transform: rotate(360deg); }
+`;
+
+const LoaderDiv = styled.div`
+  border: 16px solid #e0e0de;
+  border-top: 16px solid #5B4E77;
+  border-radius: 50%;
+  width: 75px;
+  height: 75px;
+  animation: ${spin} 1s linear infinite;
+`;
+
+const Loader = () => (
+  <LoaderDiv />
+);
+
+export default Loader;
