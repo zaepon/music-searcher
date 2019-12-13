6d1d0b89b9bb14322f72ea4323834203cea57892
Henri Lahti <miokillian@gmail.com>
2019-12-13 19:58:08 +0200

    remove redux from index.js

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
 import Main from "./containers/Main";
 
 import { ThemeProvider } from "styled-components";
 import { Theme, GlobalStyle } from "./theme";
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import reducer, { initialState } from "./store/reducer";
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 require("dotenv").config();
 
-const store = createStore(reducer, initialState, applyMiddleware(thunk));
 
 const Root = () => (
   <ThemeProvider theme={Theme}>
     <>
-      <Provider store={store}>
       <Router>
         <Route path='/' exact component={Main} />  
       </Router>
-      </Provider>
       <GlobalStyle />
     </>
   </ThemeProvider>
