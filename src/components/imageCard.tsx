9b2e393f39362d3b17cf67e46325eb7472499876
Henri Lahti <miokillian@gmail.com>
2019-12-16 23:42:16 +0200

    image card

---
+++ src/components/imageCard.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+
+import playButton from './play-button.svg';
+
+const StyledIcon = styled.div`
+  width: 50px;
+  opacity: 0;
+  position: absolute;
+  width: 50%;
+  height: 50%;
+  top: 25%;
+  left: 25%;
+  transition: .5s;
+  cursor: pointer;
+  
+`;
+
+const StyledImg = styled.img`
+  width: 250px;
+  min-height: 231px;
+`;
+
+const ImgContainer = styled.div`
+  margin-top: 1em;
+  margin-right: 2em;
+  position: relative;
+
+  &:hover {
+    opacity: 0.8;
+  }
+  &:hover ${StyledIcon} {
+    opacity: 1;
+    background-image: url(${playButton});
+    background-repeat: no-repeat;
+  }
+`
+
+interface ImageCardProps {
+  img: string
+}
+
+
+const ImageCard = (props: ImageCardProps) => {
+  return (
+    <ImgContainer>
+      <StyledImg src={props.img} />
+      <StyledIcon></StyledIcon>
+    </ImgContainer>
+  )
+}
+
+export default ImageCard;
